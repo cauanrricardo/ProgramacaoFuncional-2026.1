863e41608a67568407295bfa50f6e875142fd8df
cauanrricardo <cauanrricardo@gmail.com>
2026-03-11 16:53:41 -0300

    feat: adicao de mais funcoes e suas respectivas assinaturas

--- Treinos/aula1.hs
+++ Treinos/aula1.hs
@@ -1,15 +1,29 @@
+soma :: Num a => a -> a -> a
 soma x y = x + y
+
+dobro :: Num a => a -> a
 dobro x = 2 * x
 
 triplo :: Int -> Int
 triplo x = x * 3
 
+quadrado :: Num a => a -> a
 quadrado x = x * x
 
+ehPar :: Integral a => a -> Bool
 ehPar x = mod x 2 == 0
 
 metade :: Double -> Double
 metade x = x / 2
 
 maiorDeIdade :: Int -> Bool
-maiorDeIdade idade = idade >= 16
+maiorDeIdade idade = idade >= 18
+
+formulacirculo :: Floating a => a -> a
+formulacirculo x = x * x  *pi
+
+antecessor :: Num a => a -> a
+antecessor x = x - 1
+
+sucessor :: Num a => a -> a
+sucessor x = x + 1
