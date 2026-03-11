bf1cf4dea3f6b97bc1dadcc3a7459360207b163a
cauanrricardo <cauanrricardo@gmail.com>
2026-03-11 15:22:45 -0300

    feat: Adicao do primeiro arquivo de funcoes

---
+++ Treinos/aula1.hs
@@ -0,0 +1,15 @@
+soma x y = x + y
+dobro x = 2 * x
+
+triplo :: Int -> Int
+triplo x = x * 3
+
+quadrado x = x * x
+
+ehPar x = mod x 2 == 0
+
+metade :: Double -> Double
+metade x = x / 2
+
+maiorDeIdade :: Int -> Bool
+maiorDeIdade idade = idade >= 16
