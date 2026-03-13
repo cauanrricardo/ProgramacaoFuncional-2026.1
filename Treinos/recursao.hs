1f30dfd82011fcf87ccb40973b39ada43afc020d
cauanrricardo <cauanrricardo@gmail.com>
2026-03-13 14:23:43 -0300

    feat:Adicionar funcoes de fatorial

--- Treinos/recursao.hs
+++ Treinos/recursao.hs
@@ -5,3 +5,16 @@ fatorial n = n * fatorial(n - 1)
 4 x 3= 12
 12
 --}
+
+
+fac n = 
+        if n <= 1 then
+            1
+        else 
+            n * fac (n-1)
+
+
+somaAte 0 = 0
+somaAte x = x + somaAte(x - 1)
+{-- 5 + somaAte 4 + 3 + 2 + 1 + 0 = 15
+--}
