fac35a274aab377e4c191d367667f5c8013335eb
cauanrricardo <cauanrricardo@gmail.com>
2026-03-14 17:00:06 -0300

    feat: Adicao de novas funcoes lambda e filter/map

--- Treinos/lambda.hs
+++ Treinos/lambda.hs
@@ -22,7 +22,7 @@ bomDia = map (\nome -> "bom dia, " ++ nome) nomes
 -- Exercicios
 
 --mutiplicar por 3
-listaNumeros = [3,6,9]
+listaNumeros = [1,2,3]
 mutiplicaLista = map(\n -> n * 3) listaNumeros
 
 -- soma dois numeros
@@ -31,4 +31,32 @@ somaLambda =  somar 10 5
 
 -- elevar ao quadrado
 listaQuadrado = [2,3,4]
-listaAoQuadrado = map(\n -> n ^ 2) listaQuadrado
+listaAoQuadrado = map(\n -> n ^ 2) listaQuadrado -- ^serve pra potencia inteira
+
+-- filtarr numeros pares
+listaX = [1,2,3,4,5,6]
+pares = filter(\x -> mod x 2 == 0) listaX
+
+-- >= 10
+notas =  [1,2,3,4,16,12,34,67,89,9]
+listaMaiorQueDez = filter(\x -> x >= 10) notas
+
+-- usando map e filter juntos
+numeros = [1,2,4,6,8,10]
+resultado = map(\x -> x * 2) (filter (\x -> x > 5) numeros) -- lembrar da ordem dos () filter vem 1º
+
+-- somar 2 a cada elemento
+lista2 = [5,10,15]
+resultadoLista2 = map(\x -> x + 2) lista2
+
+--usar filter pra <= 8
+lista3 = [2,5,8,10,1,7]
+resultadoLista3 = filter(\x -> x <= 8) lista3
+
+-- filtar apenas numeros inpares e mutiplicar - los
+lista4 = [1,2,3,4,5]
+resultadoLista4 = map(\x -> x * 10) (filter(\x -> mod x 2 /= 0) lista4)
+
+-- lambda com dois argumentos
+formula = (\x y -> x * y + 1)
+resultadoFormula = formula 3 4
